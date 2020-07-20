95fdb11d25e473384fab2d669264c3a557ec9297
Dan Thompson <34109026+Laminad@users.noreply.github.com>
2020-07-20 19:18:20 -0400

    Update meraki_cisco_phone_export_clean.py

--- meraki_cisco_phone_export_clean.py
+++ meraki_cisco_phone_export_clean.py
@@ -27,16 +27,14 @@ def network_id_list_generator(net_devices):
 # A functions that takes a network id list and output file and exports 
 # the Cisco clients, network name, Meraki serial, and client MAC address to the output file.
 def cisco_device_export(net_ids, output_file):
-    with open(output_file, "r+") as f:
-        f.write("Network Name, Serial #, Manufacturer, MAC Address\n")
+        output_file.write("Network Name, Serial #, Manufacturer, MAC Address\n")
         for net_id in net_ids:
             serial = m.devices.getNetworkDevices(net_id)[0]['serial']
             network_name = m.networks.getNetwork(net_id)['name']
             site_clients = m.clients.getNetworkClients(net_id)
             for client in site_clients:
                 if search(client["manufacturer"], "Cisco Systems"):
-                    f.write("{}, {}, {}, {}\n".format(network_name, serial, client["manufacturer"], client["mac"]))
-    f.close()
+                    output_file.write("{}, {}, {}, {}\n".format(network_name, serial, client["manufacturer"], client["mac"]))
 
 
 if __name__ == "__main__":
@@ -77,23 +75,27 @@ if __name__ == "__main__":
         # The specific configuration templates IDs to use in the script.
         config_template_ids = ['N_630503947831923190', 'N_630503947831946699']
         
-        for template in config_template_ids:
+        with open(output_file, "w") as ofile:
             
-            # Getting the network_devices using the network_device_list function
-            print("{} INFO: Generating the list of network devices associated with the configuration templates.".format(dt.now()))
-            print("-"*120)
-            net_devices = network_device_list_generator(organization_id, template)
+            for template in config_template_ids:
 
-            # Getting the network IDs using the network device list generated and the network_id_list function.
-            print("{} INFO: Generating the list of network IDs associated with the network devices.".format(dt.now()))
-            print("-"*120)
-            ids = network_id_list_generator(net_devices)
-            print("{} INFO: Network ID List created successfully.".format(dt.now()))
+                # Getting the network_devices using the network_device_list function
+                print("{} INFO: Generating the list of network devices associated with the configuration templates.".format(dt.now()))
+                print("-"*120)
+                net_devices = network_device_list_generator(organization_id, template)
 
-            # Creating the Network Name, Serial, Client MAC list using the cisco_device_export function.
-            print("{} INFO: Generating the device name and mgmt IP objects associated for the specific network IDs.".format(dt.now()))
-            print("-"*120)
-            cisco_device_export(ids, output_file)
+                # Getting the network IDs using the network device list generated and the network_id_list function.
+                print("{} INFO: Generating the list of network IDs associated with the network devices.".format(dt.now()))
+                print("-"*120)
+                ids = network_id_list_generator(net_devices)
+                print("{} INFO: Network ID List created successfully.".format(dt.now()))
+
+                # Creating the Network Name, Serial, Client MAC list using the cisco_device_export function.
+                print("{} INFO: Generating the device name and mgmt IP objects associated for the specific network IDs.".format(dt.now()))
+                print("-"*120)
+                cisco_device_export(ids, output_file)
+                
+          ofile.close()
 
 
     except KeyboardInterrupt:
