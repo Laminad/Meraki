d779fd84a05094a96db6113b36ddd7202a410c65
Thompson <danthompson@presidio.com>
2020-07-21 09:59:00 -0400

    Generalized ISE Export

--- meraki_ise_export.py
+++ meraki_ise_export.py
@@ -5,8 +5,7 @@ import sys
 # A function that creates a list of devices associated with a specific device template in the Meraki dashboard.
 def network_device_list_generator(org_id, config_temp_ids):
     network_devices = []
-    for template in config_temp_ids:
-        network_devices = m.networks.getOrganizationNetworks(org_id, configTemplateId=template)
+    network_devices = m.networks.getOrganizationNetworks(org_id, configTemplateId=template)
     return network_devices
 
 # A function that takes a list network devices and creates a list of their network IDs.
@@ -20,18 +19,16 @@ def network_id_list_generator(net_devices):
 def mgmt_ip_and_device_name_list_generator(net_ids, output_file):
     mgmt_ip = ''
     device_name = ''
-    with open(output_file, 'w') as f:
-        for net_id in net_ids:
-            try:
-                serial = m.devices.getNetworkDevices(net_id)[0]['serial']
-                mgmt_ip = m.devices.getNetworkDevice(net_id, serial)['lanIp']
-                mgmt_ip = m.devices.getNetworkDevice(net_id, serial)['Ip']
-                device_name = device_name = m.networks.getNetwork(net_id)['name']
-                print("{} INFO: Writing network name and mgmt IP to the output file > {} - {}.".format(dt.now(),device_name, mgmt_ip))
-                f.write("{},{}\n".format(device_name, mgmt_ip))
-            except KeyError:
-                print("{} ERROR: Device Offline.".format(dt.now()))
-    f.close
+    for net_id in net_ids:
+        try:
+            serial = m.devices.getNetworkDevices(net_id)[0]['serial']
+            mgmt_ip = m.devices.getNetworkDevice(net_id, serial)['lanIp']
+            mgmt_ip = m.devices.getNetworkDevice(net_id, serial)['Ip']
+            device_name = device_name = m.networks.getNetwork(net_id)['name']
+            print("{} INFO: Writing network name and mgmt IP to the output file > {} - {}.".format(dt.now(),device_name, mgmt_ip))
+            output_file.write("{},{}\n".format(device_name, mgmt_ip))
+        except KeyError:
+            print("{} ERROR: Device Offline.".format(dt.now()))
 
 
 if __name__ == "__main__":
@@ -42,59 +39,53 @@ if __name__ == "__main__":
         # Unique Meraki API key from Meraki Dashboard
         api_key = input("Enter you Meraki API key: ")
 
-         
-        orgs = m.organizations.getOrganizations() 
-        for org in orgs:
-            print(org)
-        organization_id = input("Enter the Organization ID: ")
-
-        # The output file to export the device names and mgmt IPs after the script has completed.
-        output_file = "C:\\Users\\danthompson\\Documents\\LabCorp\\wfh_z3_device_name_mgmt_ip.txt"
-
         # Initiating the API session and creating API object to use in API queries
         m = meraki.DashboardAPI(api_key)
 
-        # The specific configuration templates to run the script for.
-        config_template_ids = ['N_630503947831946812', 'N_630503947831949798']
+        # Outputting the Organization IDs and asking the user to input the organization they are working on. 
+        orgs = m.organizations.getOrganizations() 
+        for org in orgs:
+            print(org)
+            
+        organization_id = input("Enter the Organization ID: ")
 
-        # This command was used to find the device template IDs
-        # m.config_templates.getOrganizationConfigTemplates(organization_id)
+        # The output file to export the device names and mgmt IPs after the script has completed.
+        output_file = input("Enter the file path of the output file: ")
 
-        '''
-                Device Configuration Template Objects Example
+        # Outputting the organization config templates and asking the user to select the
+        # specific templates they want the client information from.
+        temps = m.config_templates.getOrganizationConfigTemplates(organization_id)
+        for temp in temps:
+            print(temp)
 
-                {
-                id: N_630503947831946812
-                name: Z3C Test 11-04-2019
-                productTypes: ['appliance']
-                timeZone: America/New_York
-                }
+        config_template_ids = []
+        user_input = "L_123"
+        while user_input != "0":
+            user_input = input("Enter a configuration template ID or 0 to end: ")
+            if user_input !="0":
+                config_template_ids.append(user_input)
 
-                {
-                id: N_630503947831949798
-                name:  DEFAULT TEMPLATE - WFH Z3 NO WIFI
-                productTypes: ['appliance']
-                timeZone: America/New_York
-                }
+        with open(output_file, 'w') as ofile:
 
-        '''
+            for template in config_template_ids:
 
-        # Getting the network_devices using the network_device_list function
-        print("{} INFO: Generating the list of network devices associated with the configuration templates.".format(dt.now()))
-        print("-"*120)
-        net_devices = network_device_list_generator(organization_id, config_template_ids)
+                # Getting the network_devices using the network_device_list function
+                print("{} INFO: Generating the list of network devices associated with the configuration templates.".format(dt.now()))
+                print("-"*120)
+                net_devices = network_device_list_generator(organization_id, template)
 
-        # Getting the network IDs using the network device list generated and the network_id_list function.
-        print("{} INFO: Generating the list of network IDs associated with the network devices.".format(dt.now()))
-        print("-"*120)
-        ids = network_id_list_generator(net_devices)
-        print("{} INFO: Network ID List created successfully.".format(dt.now()))
+                # Getting the network IDs using the network device list generated and the network_id_list function.
+                print("{} INFO: Generating the list of network IDs associated with the network devices.".format(dt.now()))
+                print("-"*120)
+                ids = network_id_list_generator(net_devices)
+                print("{} INFO: Network ID List created successfully.".format(dt.now()))
 
-        # Creating the Network Name and Mgmt IP list using the mgmt_ip_and_device_list function.
-        print("{} INFO: Generating the device name and mgmt IP objects associated for the specific network IDs.".format(dt.now()))
-        print("-"*120)
-        mgmt_ip_and_device_name_list_generator(ids, output_file)
+                # Creating the Network Name and Mgmt IP list using the mgmt_ip_and_device_list function.
+                print("{} INFO: Generating the device name and mgmt IP objects associated for the specific network IDs.".format(dt.now()))
+                print("-"*120)
+                mgmt_ip_and_device_name_list_generator(ids, ofile)
 
+        ofile.close
 
     except KeyboardInterrupt:
         # Except statement is to clean up keyboard interrupt output. 
