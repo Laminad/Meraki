025355f359b5c9a22b3505944228d29363219c19
Thompson <danthompson@presidio.com>
2020-07-27 10:53:40 -0400

    New script to check PoE Status of MS devices

---
+++ meraki_poe_status.py
@@ -0,0 +1,43 @@
+from datetime import datetime as dt
+from re import match
+import meraki
+import sys
+
+
+def get_device_poe_status(serial):
+    port_status = m.switch_ports.getDeviceSwitchPortStatuses(serial)
+    return port_status
+
+
+if __name__ == "__main__":
+    
+    start_time = dt.now()
+
+    try:
+        # Unique Meraki API key from the Meraki Dashboard
+        api_key = input("Enter your Meraki API key: ")
+
+        # Initiating the API session and creating API object to use in API queries
+        m = meraki.DashboardAPI(api_key)
+        
+        # Outputting the Organization IDs and asking the user to input the organization they are working on.
+        orgs = m.organizations.getOrganizations()
+        print("\nOrganization IDs")
+        print("-"*25)
+        for org in orgs:
+            print("{}: {}".format(org['name'], org['id']))
+        organization_id = input("Enter the Organization ID: ")
+        serial_number = input("Enter the Serial number of the device to check PoE status for: ") 
+
+        get_device_poe_status(serial_number)
+
+    except KeyboardInterrupt:
+        # Except statement is to clean up keyboard interrupt output. 
+        # This stops the whole call stack from being output to the CLI.
+        print("{} ERROR: Keyboard Interrupt.".format(dt.now()))
+
+
+    # Calculating total runtime
+    end_time = dt.now()
+    total_runtime = end_time - start_time
+    print("{} INFO: Total Runtime > {}".format(dt.now(), total_runtime))
