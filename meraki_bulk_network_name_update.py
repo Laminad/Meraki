673860bfc0a868c5292bde8eee240e68414c87db
Laminad <laminad@protonmail.com>
2020-09-01 15:17:10 -0400

    reduced number of loops

--- meraki_bulk_network_name_update.py
+++ meraki_bulk_network_name_update.py
@@ -4,10 +4,9 @@ import meraki
 import sys
 
 def network_search(site_name, org_id):
-    org_networks = m.networks.getOrganizationNetworks(org_id)
-    for network in org_networks:
-        if re.search(re.compile(site_name), network['name']):
-            print(network['name'])
+    organization_networks = m.networks.getOrganizationNetworks(org_id)
+    for network in organization_networks:
+        if re.match(site_name, network['name']):
             return network['id']
 
 if __name__ == "__main__":
@@ -28,21 +27,20 @@ if __name__ == "__main__":
         for org in orgs:
             print("{}: {}".format(org['name'], org['id']))
         organization_id = input("Enter the Organization ID: ")
-
-        network_ids_to_update = []
-
         input_file = input("Enter the file of network names to update: ")
-        with open(input_file, "r") as ifile:
-            for site_name in ifile:
-                print(site_name)
-                network_id = network_search(site_name, organization_id)
-                network_ids_to_update.append(network_id)
-        ifile.close()
+        org_devices = m.devices.getOrganizationDevices(organization_id)
+        
+        networks_to_update = []
 
-        for network_id in network_ids_to_update:
-            print(network_id)
-            network_name = m.networks.getNetwork(network_id)['name']
-            response = m.networks.updateNetwork(network_id, name=network_name+'-Migrated')
+        with open(input_file, "r") as ifile:
+            for network_name in ifile:
+                print(network_name)
+                network_id = network_search(network_name, org_devices)
+                print(network_id)
+                network_name = m.networks.getNetwork(network_id)['name']
+                print(network_name)
+                response = m.networks.updateNetwork(network_id, name=network_name+'-Migrating')
+        ifile.close()
 
     except KeyboardInterrupt:
         # Except statement is to clean up keyboard interrupt output. 
