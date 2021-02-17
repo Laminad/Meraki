2d8ee365cc842c0415c8186433f773e3722c624b
Laminad <laminad@protonmail.com>
2021-02-17 11:44:56 -0500

    fixed logging output

--- meraki_mx_backup.py
+++ meraki_mx_backup.py
@@ -1,6 +1,7 @@
 from datetime import datetime as dt
 import meraki
 
+
 if __name__ == "__main__":
     
     start_time = dt.now()
@@ -29,7 +30,7 @@ if __name__ == "__main__":
             print(f"{org['name']}: {org['id']}")
         organization_id = input("Enter the Organization ID: ")
         print("\n")
-        
+
         # Asking the user to enter the file path of the text file of devices to apply templates updates to.
         input_file = r"C:\Users\danthompson\Documents\Repos\Meraki\serials.txt"
         output_file = r"C:\Users\danthompson\Documents\Repos\Meraki\mx_backup.txt"
@@ -65,84 +66,85 @@ if __name__ == "__main__":
                 try:
                     network_id = m.devices.getDevice(serial)["networkId"]
                 except:
-                    print(f"{dt.now()} ERROR: No network with serial {serial}")
+                    print(f"{dt.now().replace(microsecond=0)}       script:    ERROR > No network with serial {serial}")
                     pass
                 try:
                     network = m.networks.getNetwork(network_id)
                     device_config["network"] = network
                 except:
-                    print(f"{dt.now()} ERROR: Invalid network id {network_id}")
+                    print(f"{dt.now().replace(microsecond=0)}       script:    ERROR > Invalid network id {network_id}")
                     pass
                 try:
                     traffic_analysis = m.networks.getNetworkTrafficAnalysis(network_id)
                     device_config["traffic_analysis"] = traffic_analysis
                 except:
-                    print(f"{dt.now()} INFO: Traffic analysis not configured or disabled on {network['name']}")
+                    print(f"{dt.now().replace(microsecond=0)}       script:     INFO > Traffic analysis not configured or disabled on {network['name']}")
                 try:
                     syslog_servers = m.networks.getNetworkSyslogServers(network_id)
                     device_config["syslog_servers"] = syslog_servers
                 except:
-                    print(f"{dt.now()} INFO: No syslog servers configured on {network['name']}")
+                    print(f"{dt.now().replace(microsecond=0)}       script:     INFO > No syslog servers configured on {network['name']}")
                 try:
                     snmp_configuration = m.networks.getNetworkSnmp(network_id)
                     device_config["snmp_configuration"] = snmp_configuration
                 except:
-                    print(f"{dt.now()} INFO: No snmp configuration on {network['name']}")
+                    print(f"{dt.now().replace(microsecond=0)}       script:     INFO > No snmp configuration on {network['name']}")
                 try:
                     firmware_schedule = m.networks.getNetworkFirmwareUpgrades(network_id)
                     device_config["firmware_schedule"] = firmware_schedule
                 except:
-                    print(f"{dt.now()} INFO: No firmware upgrade schedule configured on {network['name']}")
+                    print(f"{dt.now().replace(microsecond=0)}       script:     INFO > No firmware upgrade schedule configured on {network['name']}")
                 try:
                     alerts = m.networks.getNetworkAlertsSettings(network_id)
                     device_config["alerts"] = alerts
                 except:
-                    print(f"{dt.now()} INFO: No alert configurtion applied on {network['name']}")
+                    print(f"{dt.now().replace(microsecond=0)}       script:     INFO > No alert configurtion applied on {network['name']}")
                 try:
                     vlans = m.appliance.getNetworkApplianceVlans(network_id)
                     device_config["vlans"] = vlans
                 except:
-                    print(f"{dt.now()} INFO: No vlan configuration applied on {network['name']}")
+                    print(f"{dt.now().replace(microsecond=0)}       script:     INFO > No vlan configuration applied on {network['name']}")
                 try:
                     static_routes = m.appliance.getNetworkApplianceStaticRoutes(network_id)
                     device_config["static_routes"] = static_routes
                 except:
-                    print(f"{dt.now()} INFO: No static routes configured on {network['name']}")
+                    print(f"{dt.now().replace(microsecond=0)}       script:     INFO > No static routes configured on {network['name']}")
                 try:
                     l3_firewall_rules = m.appliance.getNetworkApplianceFirewallL3FirewallRules(network_id)
                     device_config["l3_firewall_rules"] = l3_firewall_rules
                 except:
-                    print(f"{dt.now()} INFO: No l3 firewall rules configured on {network['name']}")
+                    print(f"{dt.now().replace(microsecond=0)}       script:     INFO > No l3 firewall rules configured on {network['name']}")
                 try:
                     l7_firewall_rules = m.appliance.getNetworkApplianceFirewallL7FirewallRules(network_id)
                     device_config["l7_firewall_rules"] = l7_firewall_rules
                 except:
-                    print(f"{dt.now()} INFO: No l7 Firewall rules configured on {network['name']}")
+                    print(f"{dt.now().replace(microsecond=0)}       script:     INFO > No l7 Firewall rules configured on {network['name']}")
                 try:
                     vpn_bgp_settings = m.appliance.getNetworkApplianceVpnBgp(network_id)
                     device_config["vpn_bgp_settings"] = vpn_bgp_settings
                 except:
-                    print(f"{dt.now()} INFO: No bgp configuration applied on {network['name']}")
+                    print(f"{dt.now().replace(microsecond=0)}       script:     INFO > No bgp configuration applied on {network['name']}")
                 try:
                     site_to_site_vpn = m.appliance.getNetworkApplianceVpnSiteToSiteVpn(network_id)
                     device_config["site_to_site_vpn"] = site_to_site_vpn
                 except:
-                    print(f"{dt.now()} INFO: No site to site vpn configuration applied on {network['name']}")
+                    print(f"{dt.now().replace(microsecond=0)}       script:     INFO > No site to site vpn configuration applied on {network['name']}")
                 try:
                     vpn_firewall_rules = m.appliance.getNetworkApplianceVpnVpnFirewallRules(network_id)
                     device_config["vpn_firewall_rules"] = vpn_firewall_rules
                 except:
-                    print(f"{dt.now()} INFO: No vpn firewall rules configured on {network['name']}")
+                    print(f"{dt.now().replace(microsecond=0)}       script:     INFO > No vpn firewall rules configured on {network['name']}")
                 try:
                     traffic_shaping_uplink_selection = m.appliance.getNetworkApplianceTrafficShapingUplinkSelection(network_id)
                     device_config["traffic_shaping_uplink_selection"] = traffic_shaping_uplink_selection
                 except:
-                    print(f"{dt.now()} INFO: No traffic shaping uplink policies configured on {network['name']}")
+                    print(f"{dt.now().replace(microsecond=0)}       script:     INFO > No traffic shaping uplink policies configured on {network['name']}")
                 try:
                     traffic_shaping_bandwidth_limits = m.appliance.getNetworkApplianceTrafficShapingUplinkBandwidth(network_id)
                     device_config["traffic_shaping_bandwidth_limits"] = traffic_shaping_bandwidth_limits
                 except:
-                    print(f"{dt.now()} INFO: No traffic shaping bandwidth limited configured on {network['name']}")
+                    print(f"{dt.now().replace(microsecond=0)}       script:     INFO > No traffic shaping bandwidth limited configured on {network['name']}")
+
 
                 ofile.write("{\n")
                 config_keys = device_config.keys()
@@ -151,14 +153,13 @@ if __name__ == "__main__":
                 ofile.write("}\n\n")
 
 
-
     except KeyboardInterrupt:
         # Except statement is to clean up keyboard interrupt output. 
         # This stops the whole call stack from being output to the CLI.
-        print(f"{dt.now()} ERROR: Keyboard Interrupt.")
+        print(f"{dt.now().replace(microsecond=0)}       script:    ERROR > Keyboard Interrupt.")
 
 
     # Calculating total runtime
     end_time = dt.now()
     total_runtime = end_time - start_time
-    print(f"{dt.now()} INFO: Total Runtime > {total_runtime}")
+    print(f"{dt.now().replace(microsecond=0)}       script:     INFO > Total Runtime {total_runtime}")
